987a6323bf31a551940a136acbf21fa416fdec98
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-06 22:25:01 +0700

    Tao view model cho trang home, ho tro load toan bo profile va them profile

---
+++ MyPaintApp/PaintApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using PaintApp.Core.Interfaces;
+using PaintApp_Data.Entities;
+using System;
+
+namespace PaintApp.ViewModels
+{
+    public partial class HomeViewModel : ObservableObject
+    {
+
+        private readonly IUserProfileService _profileService;
+
+        [ObservableProperty]
+        private ObservableCollection<UserProfile> profiles;
+
+        [ObservableProperty]
+        private bool isLoading;
+
+        public HomeViewModel(IUserProfileService profileService)
+        {
+            _profileService = profileService;
+            Profiles = new ObservableCollection<UserProfile>();
+            //LoadProfilesCommand.Execute(null);
+        }
+
+        [RelayCommand]
+        public async Task LoadProfiles()
+        {
+            IsLoading = true;
+            Profiles.Clear();
+
+            var list = await _profileService.GetAllProfilesAsync();
+            foreach ( var profile in list )
+            {
+                Profiles.Add(profile);
+            }
+            IsLoading = false;
+        }
+
+        [RelayCommand]
+        public async Task AddProfile(string name)
+        {
+
+            if(String.IsNullOrWhiteSpace(name))
+            {
+                return;
+            }
+
+            var profile = new UserProfile
+            {
+                UserName = name,
+            };
+
+            await _profileService.AddProfileAsync(profile);
+
+            Profiles.Add(profile);
+        }
+
+    }
+}
