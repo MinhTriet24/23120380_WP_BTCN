d169c5fb59557fce4cdecbf3f80cc80887a1d049
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-06 21:58:54 +0700

    Tao interface cho UserProfileService voi ham GetAllProfilesAsync va AddProfileAsync de test

---
+++ MyPaintApp/PaintApp/Core/Interfaces/IUserProfileService.cs
@@ -0,0 +1,21 @@
+﻿using PaintApp_Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintApp.Core.Interfaces
+{
+    public interface IUserProfileService
+    {
+
+        Task<List<UserProfile>> GetAllProfilesAsync();
+        Task AddProfileAsync(UserProfile profile);
+
+        //Task Update
+
+        //Task Delete
+
+    }
+}
