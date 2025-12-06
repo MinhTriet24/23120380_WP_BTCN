30055f753f90b264faefb149c0a7e1a36f5bb93b
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-07 04:34:27 +0700

    Them DTO ShapeData va logic de serialize hinh ve

---
+++ MyPaintApp/PaintApp/Core/Models/ShapeData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PaintApp.Core.Models
+{
+    public class ShapeData
+    {
+        public string Type { get; set; } // "Rectangle", "Ellipse", "Line", "Polygon"
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public string Fill { get; set; }
+        public string Stroke { get; set; }
+        public double StrokeThickness { get; set; }
+
+        // Lưu kiểu nét đứt (nếu có)
+        public List<double> StrokeDashArray { get; set; }
+
+        // Dành cho Line
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+
+        public List<string> Points { get; set; }
+    }
+}
