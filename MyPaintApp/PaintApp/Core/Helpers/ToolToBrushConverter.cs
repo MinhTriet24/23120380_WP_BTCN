30a5e63af7f676af5187c13d540bfb531372e2a4
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-07 00:15:51 +0700

    Xu ly doi mau nut khi nhan vao cac nut trong Drawing Page

---
+++ MyPaintApp/PaintApp/Core/Helpers/ToolToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI; 
+using PaintApp.Core.Enums;
+using System;
+
+namespace PaintApp.Core.Helpers
+{
+    public class ToolToBrushConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is ToolType currentTool && parameter is string buttonToolName)
+            {
+                if (Enum.TryParse<ToolType>(buttonToolName, out var buttonTool))
+                {
+                    if (currentTool == buttonTool)
+                    {
+                        return new SolidColorBrush(ColorHelper.FromArgb(255, 200, 230, 255));
+                    }
+                }
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
