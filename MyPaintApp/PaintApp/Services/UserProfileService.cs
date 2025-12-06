aa390c87a4956817da71d25769dc7dbfd3e7c79b
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-06 22:00:15 +0700

    Tao service de implement, trien khai hai ham GetAllProfilesAsync va AddProfileAsync

---
+++ MyPaintApp/PaintApp/Services/UserProfileService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaintApp.Core.Interfaces;
+using PaintApp_Data.Context;
+using PaintApp_Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaintApp.Services
+{
+    public class UserProfileService : IUserProfileService
+    {
+
+        private readonly AppDbContext _context;
+
+        public UserProfileService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserProfile>> GetAllProfilesAsync()
+        {
+            return await _context.UserProfiles.ToListAsync();
+        }
+
+        public async Task AddProfileAsync(UserProfile profile)
+        {
+            _context.UserProfiles.Add(profile);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
