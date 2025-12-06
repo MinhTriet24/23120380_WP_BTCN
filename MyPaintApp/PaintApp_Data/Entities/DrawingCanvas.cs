7d926024a90f6cbaf751066f580ec56c475e7845
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-07 04:07:03 +0700

    Tao file entity cho Bang ve va Bo Template

---
+++ MyPaintApp/PaintApp_Data/Entities/DrawingCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaintApp_Data.Entities
+{
+    public class DrawingCanvas
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = "Untitled";
+
+        // Kích thước canvas
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public string BackgroundColor { get; set; } = "#FFFFFF"; // Lưu mã Hex
+
+        public string SerializedShapes { get; set; } = "[]";
+
+        [ForeignKey("UserProfileId")]
+        public virtual UserProfile UserProfile { get; set; }
+    }
+}
---
+++ MyPaintApp/PaintApp_Data/Entities/ShapeTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaintApp_Data.Entities
+{
+    public class ShapeTemplate
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string SerializedData { get; set; }
+    }
+}
