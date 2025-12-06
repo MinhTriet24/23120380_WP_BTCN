32698732445e27dbc7991c18a7834c77347a0295
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-06 23:12:22 +0700

    Xu ly hien thi anh avatar cua profile thay vi hien thi ten

---
+++ MyPaintApp/PaintApp/Core/Helpers/StringToImageSourceConverter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+
+namespace PaintApp.Core.Helpers
+{
+    public class StringToImageSourceConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var path = value as string;
+
+            if (string.IsNullOrEmpty(path))
+            {
+                return null;
+            }
+
+            try
+            {
+                Uri imageUri;
+                if (path.StartsWith("ms-appx:") || path.StartsWith("http"))
+                {
+                    imageUri = new Uri(path);
+                }
+                else
+                {
+                    imageUri = new Uri("ms-appx:///" + path);
+                }
+
+                return new BitmapImage(imageUri);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
