3d50ed0998eea008487e2c4b75dccf190f75f6c4
23120380 <huynhleminhtriet5106@gmail.com>
2025-12-07 14:28:56 +0700

    Chinh sua file UserProfile service ho tro CRUD profile va su dụng no tracking de tranh xung dot

--- MyPaintApp/PaintApp/Services/UserProfileService.cs
+++ MyPaintApp/PaintApp/Services/UserProfileService.cs
@@ -9,7 +9,6 @@ namespace PaintApp.Services
 {
     public class UserProfileService : IUserProfileService
     {
-
         private readonly AppDbContext _context;
 
         public UserProfileService(AppDbContext context)
@@ -19,14 +18,30 @@ namespace PaintApp.Services
 
         public async Task<List<UserProfile>> GetAllProfilesAsync()
         {
-            return await _context.UserProfiles.ToListAsync();
+            return await _context.UserProfiles.AsNoTracking().ToListAsync();
         }
 
         public async Task AddProfileAsync(UserProfile profile)
         {
-            _context.UserProfiles.Add(profile);
+            if (profile.Id == 0)
+            {
+                _context.UserProfiles.Add(profile);
+            }
+            else
+            {
+                _context.UserProfiles.Update(profile);
+            }
             await _context.SaveChangesAsync();
         }
 
+        public async Task DeleteProfileAsync(int id)
+        {
+            var profile = await _context.UserProfiles.FindAsync(id);
+            if (profile != null)
+            {
+                _context.UserProfiles.Remove(profile);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
